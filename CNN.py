a5a40e8b9a43f9c225248c16d04d0929e657ff98
Ben Cleveland <bnjcle001@myuct.ac.za>
2023-04-27 23:38:13 +0200

    actual final versions, fully commented

--- CNN.py
+++ CNN.py
@@ -1,85 +1,90 @@
-import torch  # Main Package
-import torchvision  # Package for Vision Related ML
-import torchvision.transforms as transforms  # Subpackage that contains image transforms
-import torch.optim as optim # Optimizers
-import torch.nn as nn  # Layers
-import torch.nn.functional as F # Activation Functions
+import torch 
+import torchvision 
+import torchvision.transforms as transforms  
+import torch.optim as optim 
+import torch.nn as nn  
+import torch.nn.functional as F 
 
-import torch.optim as optim # Optimizers
+import torch.optim as optim 
 import torch.optim.lr_scheduler as lr_scheduler
 
 
-#ADD DATA AUG, ADD DROPOUT< 
+#set seed for reliable results 
 torch.manual_seed(33)
-# Create the transform sequence
+
 transform = transforms.Compose([
-    transforms.ToTensor(),  # Convert to Tensor
-    # Normalize Image to [-1, 1] first number is mean, second is std deviation
-    transforms.Normalize((0.5,), (0.5,)) 
+     # Changes the data's format to a Tensor
+    transforms.ToTensor(), 
+    #Performs transformation on the dataset with respect to the data's means and standard deviations
+    transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261])
 ])
 
-# Load MNIST dataset
-# Train
+#Load data into two groups
+# Training set
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                       download=True, transform=transform)
-# Test
+# Testing set
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                       download=True, transform=transform)
 
-# Send data to the data loaders
+
 BATCH_SIZE = 128
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
                                           shuffle=True)
-
 test_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
                                           shuffle=False)
 
 
-# Identify device
+#selects device 
 device = ("cuda" if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available()
     else "cpu"
 )
-print(f"Using {device} device")
 
 
 
 
-# Define the training and testing functions
+
+#Training function
 def train(net, train_loader, criterion, optimizer, device):
-    net.train()  # Set model to training mode.
-    running_loss = 0.0  # To calculate loss across the batches
+    net.train() 
+    running_loss = 0.0  
+    #Iterate through the data 
     for data in train_loader:
-        inputs, labels = data  # Get input and labels for batch
-        inputs, labels = inputs.to(device), labels.to(device)  # Send to device
-        optimizer.zero_grad()  # Zero out the gradients of the ntwork i.e. reset
-        outputs = net(inputs)  # Get predictions
-        loss = criterion(outputs, labels)  # Calculate loss
-        loss.backward()  # Propagate loss backwards
-        optimizer.step()  # Update weights
-        running_loss += loss.item()  # Update loss
+        inputs, labels = data  
+        inputs, labels = inputs.to(device), labels.to(device)  
+        optimizer.zero_grad()  
+        outputs = net(inputs)  
+        loss = criterion(outputs, labels)  
+        loss.backward() 
+        optimizer.step()  
+        running_loss += loss.item() 
+        #return training loss
     return running_loss / len(train_loader)
 
 
-
+#Testing function
 def test(net, test_loader, device):
-    net.eval()  # We are in evaluation mode
+    net.eval()  
     correct = 0
     total = 0
-    with torch.no_grad():  # Don't accumulate gradients
+    #zero out the gradients
+    with torch.no_grad(): 
+        #Iterate through the data 
         for data in test_loader:
             inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device) # Send to device
-            outputs = net(inputs)  # Get predictions
-            _, predicted = torch.max(outputs.data, 1)  # Get max value
+            inputs, labels = inputs.to(device), labels.to(device) 
+            outputs = net(inputs)  
+            _, predicted = torch.max(outputs.data, 1)  
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()  # How many are correct?
+            correct += (predicted == labels).sum().item()  
+            #calculate accuracy percentage
     return correct / total
 
 
 
 
-# Define the CNN architecture
+#Cnn class object
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
@@ -88,16 +93,18 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        #using batch normalization
         self.bn1 = nn.BatchNorm1d(120)
         self.fc2 = nn.Linear(120, 84)
         self.bn2 = nn.BatchNorm1d(84)
         self.fc3 = nn.Linear(84, 10)
+        #Using dropout to manage over-fitting
         self.drop = nn.Dropout(0.25)
-
+#The function that does a feed forward of the data through the network
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1) 
         x = self.drop(x)
         x = F.relu(self.bn1(self.fc1(x)))
         x = self.drop(x)
@@ -109,34 +116,38 @@ class CNN(nn.Module):
 
 
 cnn = CNN().to(device)
-
+#Define our learning rate and momentum values
 LEARNING_RATE = 1e-1
 MOMENTUM = 0.9
 
-# Define the loss function, optimizer, and learning rate scheduler
-criterion = nn.CrossEntropyLoss() # Use this if not using softmax layer
+#Loss function
+criterion = nn.CrossEntropyLoss()
+#Function that generates new evaluated parameter values
 optimizer = optim.SGD(cnn.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+#changes learning rate after 7 "steps", a variable that is incremented with each epoch
 scheduler = lr_scheduler.StepLR(optimizer, step_size = 7, gamma=0.1)
-# Train the MLP for 5 epochs
+print("15 epoch training time")
+print(f"Connecting to {device}  ")
 for epoch in range(15):
+    #obtain estimation of training loss using the train function
     train_loss = train(cnn, train_loader, criterion, optimizer, device)
     test_acc = test(cnn, test_loader, device)
-    print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}")
+    print(f"Epoch {epoch+1}: Training loss = {train_loss:.4f}, Testing accuracy = {test_acc:.4f}")
     scheduler.step()
 
 
 
 
 
-# Test on a batch of data
-with torch.no_grad():  # Don't accumlate gradients
-  cnn.eval()  # We are in evalutation mode (i.e No dropout)
-  x = [data for data in test_loader][0][0].to(device)
-  outputs = cnn(x)  # Alias for cnn.forward
 
-  # Print example output.
-  print(outputs[0])
-  print(f'Prediction: {torch.max(outputs, 1)[1][0]}')
+
+
+with torch.no_grad():  
+  cnn.eval()  
+  x = [data for data in test_loader][0][0].to(device)
+  outputs = cnn(x)  
+
+  
 
 
   
--- MLP.py
+++ MLP.py
@@ -11,26 +11,28 @@ import torch
 import torchvision  
 import torchvision.transforms as transforms 
 
+#set seed for reliable results
 torch.manual_seed(33)
-# Create the transform sequence
+
 transform = transforms.Compose([
-    #transforms.RandomHorizontalFlip(p=0.5),
-   # transforms.RandomCrop(size=(32,32), padding=0),
-    transforms.ToTensor(),  # Convert to Tensor
-    # Normalize Image to [-1, 1] first number is mean, second is std deviation
-    #transforms.Normalize((0.4914, 0.4822, 0.4465), (0,247, 0.243, 0.261)) 
+   
+    # Changes the data's format to a Tensor
+    transforms.ToTensor(),
+    
+    #Performs transformation on the dataset with respect to the data's means and standard deviations
+    
     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261])
 ])
 
-# Load MNIST dataset
-# Train
+#Load data into two groups
+# Training set
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                       download=True, transform=transform)
-# Test
+# Testing set
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                       download=True, transform=transform)
 
-# Send data to the data loaders
+
 BATCH_SIZE = 128
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
                                           shuffle=True)
@@ -42,51 +44,54 @@ test_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
 examples = enumerate(test_loader)
 batch_idx, (example_data, example_targets) = next(examples)
 
-print(example_data.shape)
 
 
 
 
 
-# Define the MLP architecture
+
+#MLP class object
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
-        self.flatten = nn.Flatten() # For flattening the 2D image
+        self.flatten = nn.Flatten() 
        
-        self.fc1 = nn.Linear(32*32*3, 2040)  # Input is image with shape (28x28)
+        self.fc1 = nn.Linear(32*32*3, 2040)  
+        #using batch normalization
         self.bn1 = nn.BatchNorm1d(2040)
 
-        self.fc2 = nn.Linear(2040, 1024)  # First HL
+        self.fc2 = nn.Linear(2040, 1024)  
         self.bn2 = nn.BatchNorm1d(1024)
 
         self.fc3 = nn.Linear(1024, 1024)
         self.bn3 = nn.BatchNorm1d(1024)
 
-        self.fc4 = nn.Linear(1024, 1024)  # First HL
+        self.fc4 = nn.Linear(1024, 1024) 
         self.bn4 = nn.BatchNorm1d(1024)
 
         self.fc5= nn.Linear(1024, 10)
         self.output = nn.LogSoftmax(dim=1)
+        #Using dropout to manage over-fitting
         self.dropout = nn.Dropout(0.15)
-
+#The function that does a feed forward of the data through the network
     def forward(self, x):
-      # Batch x of shape (B, C, W, H)
-      x = self.flatten(x) # Batch now has shape (B, C*W*H)
-      # x = self.dropout(x)
-      x = F.relu(self.bn1(self.fc1(x)))  # First Hidden Layer
+      
+      x = self.flatten(x) 
+     
+      x = F.relu(self.bn1(self.fc1(x))) 
       x = self.dropout(x)
-      x = F.relu(self.bn2(self.fc2(x)))  # Second Hidden Layer
+      x = F.relu(self.bn2(self.fc2(x)))  
       x = self.dropout(x)
       x = F.relu(self.bn3(self.fc3(x)))
       x = self.dropout(x)
       x = F.relu(self.bn4(self.fc4(x)))
       x = self.dropout(x)
-      x = self.fc5(x)  # Output Layer
-      x = self.output(x)  # For multi-class classification
-      return x  # Has shape (B, 10)
+      x = self.fc5(x)
+      x = self.output(x)  
+      return x  
     
-# Identify device
+
+#selects device 
 device = ("cuda" if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available()
     else "cpu"
@@ -97,79 +102,83 @@ print(f"Using {device} device")
 mlp = MLP().to(device)
 
 
-# Test on a batch of data
-with torch.no_grad():  # Don't accumlate gradients
-  mlp.eval()  # We are in evalutation mode
+
+with torch.no_grad(): 
+  mlp.eval()  
   x = example_data.to(device)
-  outputs = mlp(x)  # Alias for mlp.forward
+  outputs = mlp(x)  
 
-  # Print example output.
-  #print(torch.exp(outputs[0]))
-
+ 
 
 
 
-L2_lamda = 0.0001
-# Define the training and testing functions
+
+
+#Training function
 def train(net, train_loader, criterion, optimizer, device):
-    net.train()  # Set model to training mode.
-    running_loss = 0.0  # To calculate loss across the batches
+    net.train()  
+    running_loss = 0.0  
+    #Iterate through the data 
     for data in train_loader:
-        inputs, labels = data  # Get input and labels for batch
-        inputs, labels = inputs.to(device), labels.to(device)  # Send to device
-        optimizer.zero_grad()  # Zero out the gradients of the ntwork i.e. reset
-        outputs = net(inputs)  # Get predictions  (forward pass)
-        loss = criterion(outputs, labels)  # Calculate loss (calculation of error)
+        inputs, labels = data  
+        inputs, labels = inputs.to(device), labels.to(device) 
+        optimizer.zero_grad() 
+        outputs = net(inputs) 
+        loss = criterion(outputs, labels)  
 
-       # L2_norm = sum(p.pow(2.0).sum() for p in net.parameters())
-       # loss += L2_lamda * L2_norm
+      
 
-        loss.backward()  # Propagate loss backwards (backwards propagate)
-        optimizer.step()  # Update weights  (adjust weight)
-        running_loss += loss.item()  # Update loss
+
+        loss.backward()  
+        optimizer.step() 
+        running_loss += loss.item()  
+        #return training loss
     return running_loss / len(train_loader)
 
-
+#Testing function
 def test(net, test_loader, device):
-    net.eval()  # We are in evaluation mode
+    net.eval() 
     correct = 0
     total = 0
-    with torch.no_grad():  # Don't accumulate gradients
+    #zero out the gradients
+    with torch.no_grad():  
+        #Iterate through the data 
         for data in test_loader:
             inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device) # Send to device
-            outputs = net(inputs)  # Get predictions
-            _, predicted = torch.max(outputs.data, 1)  # Get max value
+            inputs, labels = inputs.to(device), labels.to(device) 
+            outputs = net(inputs) 
+            _, predicted = torch.max(outputs.data, 1)  
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()  # How many are correct?
+            correct += (predicted == labels).sum().item() 
+    #calculate accuracy percentage
     return correct / total
 
 
 mlp = MLP().to(device)
-
+#Define our learning rate and momentum values
 LEARNING_RATE = 0.01
 MOMENTUM = 0.9
 
-# Define the loss function, optimizer, and learning rate scheduler
+#Loss function
 criterion = nn.NLLLoss()
-#optimizer = optim.SGD(mlp.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=L2_lamda)
+#Function that generates new evaluated parameter values
 optimizer = optim.SGD(mlp.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
-
+#changes learning rate after 6"steps", a variable that is incremented with each epoch
 scheduler = lr_scheduler.StepLR(optimizer, step_size = 6, gamma=0.1)
 
-# Train the MLP for 5 epochs
+
 for epoch in range(15):
-    
+    #obtain estimation of training loss using the train function
     train_loss = train(mlp, train_loader, criterion, optimizer, device)
     test_acc = test(mlp, test_loader, device)
     print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}")
     scheduler.step()
 
 
-# Test on a batch of data
-with torch.no_grad():  # Don't accumlate gradients
-  mlp.eval()  # We are in evalutation mode
-  x = example_data.to(device)
-  outputs = mlp(x)  # Alias for mlp.forward
 
-  # Print example output.
+with torch.no_grad():  
+  mlp.eval() 
+  x = example_data.to(device)
+  outputs = mlp(x)  
+
+
